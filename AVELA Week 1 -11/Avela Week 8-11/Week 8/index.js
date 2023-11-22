f3cc592d64c90f8ad2ac76c44acdaadfb2265b5e
SaaraUthmaan <149133492+SaaraUthmaan@users.noreply.github.com>
2023-11-21 22:07:05 -0800

    Add files via upload

---
+++ AVELA Week 1 -11/Avela Week 8-11/Week 8/index.js
@@ -0,0 +1,2 @@
+// Get all book buttons
+const bookBtns = document.querySelectorAll('.tour-btn');
---
+++ AVELA Week 1 -11/Avela Week1-4/JSfirst.js
@@ -0,0 +1,12 @@
+window.alert("Do you want to enter?");
+alert("I warn you it's risky!");
+function deleteEverything(){
+  document.getElementById('everything').style.display = 'none';
+}
+/*
+document.getElementById('ok').style.display = 'none';
+  document.getElementById('better').style.display = 'none';
+  document.getElementById('poem').style.display = 'none';
+  document.getElementById('fight').style.display = 'none';
+This was the extra code I had before I decided to use a div
+*/
